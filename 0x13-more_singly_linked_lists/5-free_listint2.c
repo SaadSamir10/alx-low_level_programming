58c21803788dd3a9053a708168730b5b701efd2e
SaadSamir10 <saadsamirrakha123123@gmail.com>
2023-08-29 20:34:13 +0300

    linkedlist

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,22 @@
 /**
  *free_listint2 - frees a list
  *@head: a pointer point to the head of a list
+ *Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *node;
+	listint_t *node, *t;
+
+	if (!head)
+		return;
 
 	while (head)
 	{
-		node = head;
-		head = head->next;
-		free(node);
+		t = node;
+		node = node->next;
+		free(t);
 	}
+
+	*head = NULL;
 }
