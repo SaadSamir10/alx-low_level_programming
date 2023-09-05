5eec5aa6f1b3372c1e100459bb9471b68ef6d5ef
SaadSamir10 <saadsamirrakha123123@gmail.com>
2023-09-05 04:41:13 +0300

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *read_textfile - reads a text file and prints it
+ *@filename: name of filename
+ *@letters: number of letters read
+ *
+ *Return: data read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t data;
+	char text[1024 * 8];
+
+	if (!filename || !letters)
+		return (0);
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+		return (0);
+	data = read(f, &text, data);
+	close(f);
+	return (data);
+}
