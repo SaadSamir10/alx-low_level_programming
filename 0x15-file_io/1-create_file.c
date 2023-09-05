e1efeba940cfea79f0b37de5b34b6412a22078e0
SaadSamir10 <saadsamirrakha123123@gmail.com>
2023-09-05 05:04:56 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,13 @@ int create_file(const char *filename, char *text_content)
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
-	while (text_content[len] != '\0')
+	while (text_content[len] != NULL)
 		len++;
 	writen = write(f, text_content, len);
-	if (writen != len || writen == -1)
+	if (writen != len)
 		return (-1);
-	close(f);
+	writen = close(f)
+	if (writen == -1)
+		exit(-1);
 	return (1);
 }
